1b64302cad645eac04b2aa8f8b46830fdd4cfad2
Gitmohbility <mohbility.pro@gmail.com>
2025-06-07 01:18:11 -0500

    Initial configuration files

---
+++ parameters.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+    'database_host' => 'localhost',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => '',
+    'mailer_password' => '',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2024-06-07',
+    'secret' => 'your-secret-key-here',
+    'cookie_key' => 'your-cookie-key-here',
+    'cookie_iv' => 'your-cookie-iv-here',
+    'new_cookie_key' => ''
+);
